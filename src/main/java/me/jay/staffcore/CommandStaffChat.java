eee2159e62748c73c99e4e28d044965c3ab603f0
oddmario <mariolatif741@yandex.com>
2022-02-02 21:41:38 +0200

    Fix the event listener & respect the toggle

--- src/main/java/me/jay/staffcore/CommandStaffChat.java
+++ src/main/java/me/jay/staffcore/CommandStaffChat.java
@@ -16,23 +16,25 @@ public class CommandStaffChat implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
-            if(player.isOp() || player.hasPermission(config.getString("StaffChat.Permission"))) {
-                if(args.length <= 0) {
-                    player.sendMessage(ChatColor.RED + "Correct usage: /sc <message>");
-                } else {
-                    String message = "";
-                    for (String arg : args) {
-                        message = message + arg + " ";
-                    }
-                    if(message.length() <= 0) {
+            if(config.getBoolean("Features.StaffChat")) {
+                if(player.isOp() || player.hasPermission(config.getString("StaffChat.Permission"))) {
+                    if(args.length <= 0) {
                         player.sendMessage(ChatColor.RED + "Correct usage: /sc <message>");
                     } else {
-                        staffchat.StaffChatMessage(player, message);
-                        return true;
+                        String message = "";
+                        for (String arg : args) {
+                            message = message + arg + " ";
+                        }
+                        if(message.length() <= 0) {
+                            player.sendMessage(ChatColor.RED + "Correct usage: /sc <message>");
+                        } else {
+                            staffchat.StaffChatMessage(player, message);
+                            return true;
+                        }
                     }
+                } else {
+                    player.sendMessage(ChatColor.RED + "[ERROR] You don't have permission to run this command.");
                 }
-            } else {
-                player.sendMessage(ChatColor.RED + "[ERROR] You don't have permission to run this command.");
             }
         } else {
             StaffCore.getInstance().getLogger().info(ChatColor.RED + "[StaffCore] [ERROR] This command must be executed as a player.");
--- src/main/java/me/jay/staffcore/EventOnMessage.java
+++ src/main/java/me/jay/staffcore/EventOnMessage.java
@@ -3,17 +3,18 @@ package me.jay.staffcore;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class EventOnMessage {
+public class EventOnMessage implements Listener {
     
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent e) {
         Player player = e.getPlayer();
         String message = e.getMessage();
         FileConfiguration config = StaffCore.getInstance().getConfig();
-
-        if(message.startsWith(config.getString("StaffChat.Prefix"))) {
+        
+        if(message.startsWith(config.getString("StaffChat.Prefix")) && config.getBoolean("Features.StaffChat")) {
             if(player.isOp() || player.hasPermission(config.getString("StaffChat.Permission"))) {
                 String msg = message.split(config.getString("StaffChat.Prefix"))[1];
                 if(msg.length() >= 1) {

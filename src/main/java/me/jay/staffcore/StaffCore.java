6de40db59444843b5db4b266dd9582c7e98172ec
Jay <jeffreybennett@live.co.uk>
2022-02-02 15:40:48 +0000

    config

--- src/main/java/me/jay/staffcore/StaffCore.java
+++ src/main/java/me/jay/staffcore/StaffCore.java
@@ -1,5 +1,6 @@
 package me.jay.staffcore;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StaffCore extends JavaPlugin {
@@ -10,8 +11,34 @@ public final class StaffCore extends JavaPlugin {
 
     }
 
+    public void commands(){
+
+    }
+
+    public void files(){
+
+    }
+
+    public void listeners(){
+
+    }
+
+    public void database(){
+
+    }
+
+    public void startup(){
+        this.getLogger().info("&8[&cStaffCore&8] &aYour core is now Active.");
+        this.getLogger().info("&7Created by Jayie and Mario.");
+    }
+
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private String Color(String s){
+        s = ChatColor.translateAlternateColorCodes('&',s);
+        return s;
     }
 }

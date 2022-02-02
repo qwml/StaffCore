4b25a5edbde64839cdafa773c6245236009a8a54
oddmario <mariolatif741@yandex.com>
2022-02-02 21:31:17 +0200

    Remove false returns

--- src/main/java/me/jay/staffcore/CommandStaffChat.java
+++ src/main/java/me/jay/staffcore/CommandStaffChat.java
@@ -19,27 +19,26 @@ public class CommandStaffChat implements CommandExecutor {
             if(player.isOp() || player.hasPermission(config.getString("StaffChat.Permission"))) {
                 if(args.length <= 0) {
                     player.sendMessage(ChatColor.RED + "Correct usage: /sc <message>");
-                    return false;
                 } else {
                     String message = "";
-                    for (int i = 0; i < args.length; i++) {
-                        message = message + args[i] + " ";
+                    for (String arg : args) {
+                        message = message + arg + " ";
                     }
                     if(message.length() <= 0) {
                         player.sendMessage(ChatColor.RED + "Correct usage: /sc <message>");
-                        return false;
+                    } else {
+                        staffchat.StaffChatMessage(player, message);
+                        return true;
                     }
-                    staffchat.StaffChatMessage(player, message);
-                    return true;              
                 }
             } else {
                 player.sendMessage(ChatColor.RED + "[ERROR] You don't have permission to run this command.");
-                return false;
             }
         } else {
             StaffCore.getInstance().getLogger().info(ChatColor.RED + "[StaffCore] [ERROR] This command must be executed as a player.");
-            return false;
         }
+
+        return true;
     }
     
 }

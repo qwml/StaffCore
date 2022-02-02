b028adbcb67721f3628bd971fc3ad8a9e92b0047
Jay <jeffreybennett@live.co.uk>
2022-02-02 14:50:19 +0000

    start of code

---
+++ src/main/java/me/jay/staffcore/StaffCore.java
@@ -0,0 +1,17 @@
+package me.jay.staffcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

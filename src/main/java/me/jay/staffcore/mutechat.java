6b9e2cd8b455df79753d92e3ee7c2961f1168a23
Jay <jeffreybennett@live.co.uk>
2022-02-06 03:43:11 +0000

    started mutechat

--- src/main/java/me/jay/staffcore/mutechat.java
+++ src/main/java/me/jay/staffcore/mutechat.java
@@ -1,4 +1,72 @@
 package me.jay.staffcore;
 
-public class mutechat {
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class mutechat implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        FileConfiguration config = StaffCore.getInstance().getConfig();
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+
+            if (config.getBoolean("Features.MuteChat")){
+
+                if (command.getName().equalsIgnoreCase("mutechat")){
+                    if (player.hasPermission(config.getString("MuteChat.Permission"))){
+
+                        PreparedStatement mutechatps;
+                        PreparedStatement mutechatcheckps;
+                        try {
+                            mutechatps = StaffCore.getInstance().DB.getConnection().prepareStatement("UPDATE SCServer(MutedChat) VALUES (?)");
+                            mutechatcheckps = StaffCore.getInstance().DB.getConnection().prepareStatement("SELECT MutedChat FROM SCServer");
+                            ResultSet mutechatcheckrs = mutechatcheckps.executeQuery();
+                            int mutechatcheck = -1;
+                            String message = null;
+                            if (mutechatcheckrs.next()){
+                                mutechatcheck = mutechatcheckrs.getInt("MutedChat");
+                            }
+
+                            if (mutechatcheck == 1) {
+                                mutechatps.setInt(1, 1);
+                                message = Color(config.getString("MutedChat.Format"))
+                                player.sendMessage(Color(config.getString("MutedChat.Format")));
+                            }else{
+                                mutechatps.setInt(1, 0);
+                                message = Color(config.getString("MutedChat.Format"))
+                                player.sendMessage(Color(config.getString("MutedChat.Format")));
+                            }
+
+
+                            mutechatps.executeUpdate();
+
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }
+
+            }
+
+        }
+
+        return true;
+    }
+
+    private String Color(String s){
+        s = ChatColor.translateAlternateColorCodes('&',s);
+        return s;
+    }
 }
